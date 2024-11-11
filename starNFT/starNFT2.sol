88fe913b8b0c07e7f434ae3d6fbf4e96bc35b1ac
onepaysolutions <admin@onepay.markets>
2024-11-12 02:33:02 +0800

    Update starNFT2.sol

--- starNFT/starNFT2.sol
+++ starNFT/starNFT2.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
+import "./StarNFTBase.sol";
+
+contract Star2NFT is StarNFTBase {
+    constructor(address initialOwner) 
+        StarNFTBase(
+            "Star2 NFT",
+            "STAR2",
+            initialOwner,
+            1000e6,   // 1000 USDC activation price
+            550e6,    // 550 USDC OPS presale amount
+            8         // 8 reward levels
+        ) {}
+} 
