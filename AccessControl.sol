1ad41052db2f80826ba85c99420c072647f3489a
onepaysolutions <admin@onepay.markets>
2024-11-12 02:35:45 +0800

    Update AccessControl.sol

--- AccessControl.sol
+++ AccessControl.sol
@@ -1,1 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract AccessControl is Ownable, Pausable {
+    mapping(address => bool) public operators;
+    mapping(address => bool) public managers;
+    
+    event OperatorAdded(address operator);
+    event OperatorRemoved(address operator);
+    event ManagerAdded(address manager);
+    event ManagerRemoved(address manager);
+
+    modifier onlyOperator() {
+        require(operators[msg.sender] || owner() == msg.sender, "Not operator");
+        _;
+    }
+
+    modifier onlyManager() {
+        require(managers[msg.sender] || owner() == msg.sender, "Not manager");
+        _;
+    }
+
+    constructor(address initialOwner) Ownable(initialOwner) {
+    }
+
+    function addOperator(address operator) external onlyOwner {
+        operators[operator] = true;
+        emit OperatorAdded(operator);
+    }
+
+    function removeOperator(address operator) external onlyOwner {
+        operators[operator] = false;
+        emit OperatorRemoved(operator);
+    }
+
+    function addManager(address manager) external onlyOwner {
+        managers[manager] = true;
+        emit ManagerAdded(manager);
+    }
+
+    function removeManager(address manager) external onlyOwner {
+        managers[manager] = false;
+        emit ManagerRemoved(manager);
+    }
+} 
